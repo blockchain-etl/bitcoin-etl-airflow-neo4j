4e9a3db634614efc60e82320d2360e73cb89a352
Toni Cebrián <toni.cebrian@gmail.com>
2019-04-04 11:13:15 +0200

    Several fixes

--- batch-import/bigquery/outputs.sql
+++ batch-import/bigquery/outputs.sql
@@ -10,9 +10,8 @@ SELECT
   i.transaction_hash
 FROM `bigquery-public-data.crypto_bitcoin.outputs` AS o
 LEFT JOIN `bigquery-public-data.crypto_bitcoin.inputs` AS i
-  ON o.transaction_hash = i.transaction_hash AND
+  ON o.transaction_hash = i.spent_transaction_hash AND
      o.index = i.spent_output_index
 WHERE o.block_timestamp < '2010-01-01'
-  AND i.block_timestamp < '2010-01-01'
   AND o.transaction_hash NOT IN ('d5d27987d2a3dfc724e359870c6644b40e497bdc0589a033220fe15429d88599',
                                  'e3bf3d07d4b0375638d5f1db5255fe07ba2c4cb067cd81b84ee974b6585fb468')
