8683e40d5a3ec6dcb52f9272ca986a856693c69a
Toni Cebrián <toni.cebrian@gmail.com>
2019-05-01 06:44:48 +0200

    Request whole uri instead of filename

--- batch-import/load-bitcoin-db.sh
+++ batch-import/load-bitcoin-db.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -e
 
-for var in PROJECT DUMPNAME INSTANCE; do
+for var in PROJECT DUMPFILEURI INSTANCE; do
     if [[ -z "${!var:-}" ]];
     then
         echo "You need to provide a value for env variable $var"
@@ -16,7 +16,7 @@ LOAD_CMD="sudo -u neo4j rm -rf /var/lib/neo4j/data/databases/graph.db && sudo -u
 SSH_CMD="gcloud compute --project $PROJECT ssh --zone us-central1-a"
 
 echo "$(date -Iseconds) Downloading dump from bucket to neo4j-enterprise-causal-cluster-1-core-vm-$INSTANCE"
-$SSH_CMD "neo4j-enterprise-causal-cluster-1-core-vm-$INSTANCE" --command "gsutil cp gs://$PROJECT/dumps/$DUMPNAME /tmp/bitcoin.dump"
+$SSH_CMD "neo4j-enterprise-causal-cluster-1-core-vm-$INSTANCE" --command "gsutil cp $DUMPFILEURI /tmp/bitcoin.dump"
 echo "$(date -Iseconds) Downloading dump from bucket to neo4j-enterprise-causal-cluster-1-core-vm-$INSTANCE"
 $SSH_CMD "neo4j-enterprise-causal-cluster-1-core-vm-$INSTANCE" --command "$LOAD_CMD"
 
