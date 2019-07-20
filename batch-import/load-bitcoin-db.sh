2c727cf69aad8d5eff9c84ca832920713e6b1b94
Toni Cebrián <toni.cebrian@gmail.com>
2019-07-20 08:24:04 +0200

    Fix comments

--- batch-import/load-bitcoin-db.sh
+++ batch-import/load-bitcoin-db.sh
@@ -16,7 +16,7 @@ LOAD_CMD="sudo -u neo4j rm -rf /var/lib/neo4j/data/databases/graph.db && sudo -u
 SSH_CMD="gcloud compute --project $PROJECT ssh --zone us-central1-a"
 
 echo "$(date -Iseconds) Downloading dump from bucket to neo4j-enterprise-causal-cluster-1-core-vm-$INSTANCE"
-$SSH_CMD "neo4j-enterprise-causal-cluster-1-core-vm-$INSTANCE" --command "gsutil cp $DUMPFILEURI /tmp/bitcoin.dump"
-echo "$(date -Iseconds) Downloading dump from bucket to neo4j-enterprise-causal-cluster-1-core-vm-$INSTANCE"
+$SSH_CMD "neo4j-enterprise-causal-cluster-1-core-vm-$INSTANCE" --command "rm /tmp/bitcoin.dump && gsutil cp $DUMPFILEURI /tmp/bitcoin.dump"
+echo "$(date -Iseconds) Loading dump from tmp directory to the DB in neo4j-enterprise-causal-cluster-1-core-vm-$INSTANCE"
 $SSH_CMD "neo4j-enterprise-causal-cluster-1-core-vm-$INSTANCE" --command "$LOAD_CMD"
 
